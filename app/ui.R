3933326c1eafd6e4899c94871c7726ec0621a81e
Jonathan Hill <jon.mark.hill@gmail.com>
2015-10-29 10:27:06 -0400

    Update ui.R

--- app/ui.R
+++ app/ui.R
@@ -1,38 +1,41 @@
 shinyUI(
-        navbarPage("Consumer Complaints", theme = "mycss.css",
-                tabPanel("CFPB Alone",
-                        fluidPage(fluidRow(column(width=10,offset = 1,
-                                p("Given the number of complaints per company in their consumer complaints database, the Consumer Financial Protection Bureau (CFPB) cannot tell if that number is indicative of a company's negligence or its size.  The best they can often do is draw an arbitrary cut-off, such as the mean number of complaints per company and judge each company's total number against that baseline, starting at the top and working their way down."),
-                                br(),
-                                plotOutput("plot1"),
-                                br(),
-                                dataTableOutput('dt1')
-                                )
-                        )
-                )
+    navbarPage("Consumer Complaints", theme = "mycss.css", 
+        tabPanel("Introduction", 
+            fluidPage(fluidRow(column(includeMarkdown("about.Rmd"), width = 7),
+                               column(br(), includeMarkdown("menus.Rmd"), width = 4, offset = 1)
+            ))
         ),
-                tabPanel("With SEC",
-                         fluidPage(fluidRow(column(width=10,offset = 1,
-                                p("By incorporating information from the Securities and Exchange Commission (SEC), the CFPB could standardize the number of complaints by the size of a company's assets.  Then the CFPB could focus resources on companies that are more likely to violate consumer rights given the way that company has deployed its current assets."),
-                                br(),
-                                plotOutput("plot2"),
-                                br(),
-                                dataTableOutput('dt2')
-                                )
-                        )
-                )
-        ), 
-        navbarMenu("About", 
-            tabPanel("Backend",
-                 fluidPage(
-                     includeMarkdown("meetup_app_backend.Rmd")
-                     )
-                 ),
-        tabPanel("SEC Readme",
-                 fluidPage(
-                     h1("SEC Readme")
+        navbarMenu("CFPB",
+            tabPanel("CFPB Alone",
+                fluidPage(fluidRow(column(width = 10, offset = 1,
+                    plotOutput("plot1"),
+                    br(),
+                    dataTableOutput('dt1'),
+                    br()
+                    ))
                 )
             )
+        ),
+        navbarMenu("SEC",
+            tabPanel("CFPB with SEC",
+                fluidPage(fluidRow(column(width = 10, offset = 1,
+                    plotOutput("plot2"),
+                    br(),
+                    dataTableOutput('dt2'),
+                    br()
+                    ))
+                )
+            )
+        ),
+        navbarMenu("About",
+                   tabPanel("Backend",
+                            fluidPage(includeMarkdown("meetup_app_backend.Rmd"))),
+                   tabPanel("CFPB API",
+                            fluidPage(includeMarkdown("meetup_app_backend_cfpb_api.Rmd"))),
+                   tabPanel("Financial Statements Data",
+                            fluidPage(includeMarkdown("meetup_app_backend_FS.Rmd"))),
+                   tabPanel("The Merge",
+                            fluidPage(includeMarkdown("meetup_app_backend_merge.Rmd")))
         )
     )
 )
